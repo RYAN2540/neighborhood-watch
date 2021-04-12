04fef4e9f71f6d9955119d107d9109f332029457
Your Name <you@example.com>
2021-04-12 09:20:41 +0300

    test admin and occupant models

--- neighborhoodapp/tests.py
+++ neighborhoodapp/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Admin, Amenity, Post, Neighbourhood, Occupant, Business
 
-# Create your tests here.
+class AdminTestClass(TestCase):
+    def setUp(self):
+        self.ryan = User(username = "ryan", email = "ryan@gmail.com",password = "1234")
+        self.admin = Admin(user= self.ryan)
+        self.ryan.save()
+        self.admin.save()
+
+    def tearDown(self):
+        Admin.objects.all().delete()
+        User.objects.all().delete()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.ryan, User))
+        self.assertTrue(isinstance(self.admin, Admin))
+
+class OccupantTestClass(TestCase):
+    def setUp(self):
+        self.ryan = User(username = "ryan", email = "ryan@gmail.com",password = "1234")
+        self.occupant = Occupant(user= self.ryan, profile_pic='mepng')
+        self.ryan.save()
+        self.occupant.save()
+
+    def tearDown(self):
+        Occupant.objects.all().delete()
+        User.objects.all().delete()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.ryan, User))
+        self.assertTrue(isinstance(self.occupant, Occupant))
