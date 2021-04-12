6948a434858ea6ec2f0b99a13ad3c6166d76c350
Your Name <you@example.com>
2021-04-12 12:53:00 +0300

    create my hood view function

--- neighborhoodapp/urls.py
+++ neighborhoodapp/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns=[
     re_path(r'^$',views.home,name = 'home'),
+    re_path(r'^email/$',views.email,name = 'email'),
+    re_path(r'^create-profile-admin/$',views.create_profile_admin,name = 'create-profile-admin'),
+    re_path(r'^create-hood/$',views.create_hood,name = 'create-hood'),
+    re_path(r'^update-hood/$',views.update_hood,name = 'update-hood'),
+    re_path(r'^my-admin-profile/$',views.my_admin_profile,name = 'my-admin-profile'),
+    re_path(r'^add-resident/$',views.add_resident,name = 'add-resident'),
 ]
