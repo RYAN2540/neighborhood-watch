fb4e7b313a32738febb1aac52502f664e90cae37
Your Name <you@example.com>
2021-04-12 11:07:53 +0300

    add create profile admin template

--- neighborhoodapp/urls.py
+++ neighborhoodapp/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns=[
     re_path(r'^$',views.home,name = 'home'),
     re_path(r'^email/$',views.email,name = 'email'),
+    re_path(r'^create_profile_admin/$',views.create_profile_admin,name='create_profile_admin'),
 ]
